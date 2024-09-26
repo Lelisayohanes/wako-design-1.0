6c7919d7f6253462302d14379cee824a7c457c4b
Lelisayohanes <lelisayohanes1991@gmail.com>
2024-09-26 22:23:37 +0300

    thiscommitistotestifcommentofcommitwithoutspaceandworkornot

---
+++ app/carts/page.tsx
@@ -0,0 +1,13 @@
+import { CartPageComponent } from '@/components/cart-page'
+import { ProductGridComponent } from '@/components/components-product-grid'
+import React from 'react'
+
+const OrderDetailPage = () => {
+  return (
+    <div>
+      <CartPageComponent/>
+    </div>
+  )
+}
+
+export default OrderDetailPage
